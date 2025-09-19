bfe776123db45e19b38dc68974cd762b0b268a00
hissapinto <isabelahissap@gmail.com>
2025-09-19 07:48:03 -0300

    Initial Commit

---
+++ GameOfLife/GameOfLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfLifeApp.swift
+//  GameOfLife
+//
+//  Created by Isabela Hissa Pinto on 19/09/25.
+//
+
+import SwiftUI
+
+@main
+struct GameOfLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
